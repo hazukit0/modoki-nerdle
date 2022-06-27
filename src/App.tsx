11106e4fae7a42748f364f85d6a0db71dc63fd17
YUMAKI <y.umaki@rainbow.co.jp>
2022-06-28 06:43:25 +0900

    [Update #6] Create Game Logic of Nerdle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react'
-import { Repeat } from 'typescript-tuple'
 import logo from './logo.svg';
 import './App.css';
 
@@ -158,31 +157,6 @@ interface GameLogicInterface {
   clickOperationBoard(operation: string) : boolean
 }
 
-// nerdle game logic
-class Nerdle implements GameLogicInterface {
-  operations = [['Enter','Delete']]
-  state = {
-    squares: [
-      [{value: '', class: [''],}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}], 
-      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
-    ],
-    isFinished: false,
-    activeSquare: [0,0]
-  } as GameState
-  
-  message = ''
-  
-  // click on the grit square
-  clickBoard(i: number, j:number) : boolean {
-    return true
-  }
-  
-  // click on the operation button
-  clickOperationBoard(operation: string) : boolean {
-    return true
-  }
-}
-
 // Application start point
 const App = () => {
   var logic = new Nerdle;
@@ -197,3 +171,164 @@ function log(v : any) {
 
 
 export default App;
+
+//////////////////////////////////////////////////////////////////////////////
+// GameLogicInterface implementation
+// Replacing here changes the logic of the game
+//////////////////////////////////////////////////////////////////////////////
+
+// html class
+const CLASS_ACTIVE = 'active'
+// message
+const MESSAGE_ERROR_NO_COMPUTE = 'That guess doesn\'t compute!'
+const MESSAGE_ERROR_BLANKS     = 'That guess doesn\'t compute - Please foll in the blanks!'
+const MESSAGE_YOU_WON          = 'You won!'
+const MESSAGE_YOU_LOST         = 'You lost, the calculation was '
+
+// nerdle game logic
+class Nerdle implements GameLogicInterface {
+  // todo forgot to color the control buttons
+  operations = [['1','2','3','4','5','6','7','8','9','0'],['+','-','*','/','='],['Enter','Delete']]
+  state = {
+    squares: [
+      [{value: '', class: [CLASS_ACTIVE],}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}], 
+      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
+      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
+      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
+      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
+      [{value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']},{value: '', class: ['']}, {value: '', class: ['']}, {value: '', class: ['']}],
+    ],
+    isFinished: false,
+    activeSquare: [0,0]
+  } as GameState
+  
+  // todo Multiple result
+  result = '2*3+5=11'
+  // resutlts = ['2*3+5=11','3*2+5=11','5+3*2=11']
+  message = ''
+  
+  // click on the grit square
+  clickBoard(i: number, j:number) : boolean {
+    if (this.state.isFinished) return false
+    
+    this.message = ''
+    if (this.state.activeSquare[0] != i) {
+      return false
+    }
+    let beforeActiveSquare = this.state.activeSquare
+    this.state.activeSquare = [i,j]
+    // change active position
+    if (beforeActiveSquare[1] != j) {
+      this.state.squares[beforeActiveSquare[0]][beforeActiveSquare[1]].class[0] = ''
+    }
+    this.state.squares[i][j].class[0] = CLASS_ACTIVE;
+    return true
+  }
+  
+  // click on the operation button
+  clickOperationBoard(operation: string) : boolean {
+    if (this.state.isFinished) return false
+    this.message = ''
+    const is_inputs = this.operations[0].includes(operation) || this.operations[1].includes(operation)
+    const is_operators = this.operations[2].includes(operation)
+    
+    if (!is_inputs && !is_operators) return false
+    
+    // todo refactoring
+    if (is_inputs && this.state.activeSquare[1] < this.state.squares[0].length) {
+      log('comman:'+operation)
+      // Change active position
+      this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].value = operation
+      if (this.state.activeSquare[1] < (this.state.squares[0].length - 1)) {
+        this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].class[0] = ''
+        this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]+1].class[0] = CLASS_ACTIVE
+        this.state.activeSquare[1]++
+      }
+    } else if (is_operators) {
+      switch (operation) {
+        case 'Enter':
+          log('comman:Enter')
+          // Check the result
+          const resultArray = this.state.squares[this.state.activeSquare[0]]
+          const values = resultArray.map(square => square.value as string)
+          if ( this.inputCheck(values) ) {
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].class[0] = ''
+            this.setStyle(resultArray)
+          }
+          if (values.join('') == this.result) {
+            // won
+            this.state.isFinished = true
+            this.message = MESSAGE_YOU_WON
+          } else if (this.state.activeSquare[0] < (this.state.squares.length - 1)) {
+            // next row
+            this.state.activeSquare[0]++
+            this.state.activeSquare[1] = 0
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].class[0] = CLASS_ACTIVE
+          } else {
+            // Game over
+            this.message = MESSAGE_YOU_LOST + this.result
+          }
+          break;
+        case 'Delete':
+          log('comman:Delete')
+          // delete
+          if (this.state.activeSquare[1] >= 1 && this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].value == '') {
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].class[0] = ''
+            this.state.activeSquare[1]--
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].value = ''
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].class[0] = CLASS_ACTIVE
+          } else {
+            this.state.squares[this.state.activeSquare[0]][this.state.activeSquare[1]].value = ''
+          }
+          break;
+      }
+    }
+    return true
+  }
+
+  // input check
+  private inputCheck(array : string[]) : boolean {
+    // empty check
+    if (array.indexOf('') >= 0) {
+      this.message = MESSAGE_ERROR_BLANKS
+      return false
+    }
+    // check for calculability
+    const str = array.join('')
+    if (!this.checkCalculation(str)) {
+      this.message = MESSAGE_ERROR_NO_COMPUTE
+      return false
+    }
+    return true
+  }
+  
+  // check for calculability
+  private checkCalculation(equation : string) : boolean {
+    const parts = equation.split('=')
+    if (parts.length != 2) return false
+    let leftResult = 0
+    let rightResult = 0
+    try {
+      leftResult = Function('return ('+parts[0]+');')();
+      rightResult = Function('return ('+parts[1]+');')();
+    } catch (e) {
+      return false
+    }
+    return (leftResult == rightResult)
+  }
+  
+  // set cell background from results
+  private setStyle(resultArray: SquareState[]) {
+    for (var i = 0; i < resultArray.length; i++) {
+      const v = resultArray[i]
+      if (v.value == this.result[i]) {
+        v.class = ['green']
+      } else  if (this.result.indexOf(v.value) >= 0) {
+        v.class = ['purple']
+      } else {
+        v.class = ['black']
+      }
+    }
+  }
+  
+}
